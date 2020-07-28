b2ba6613b7eb0ddfe4ada8f177a53e7607559f1d
Siaoja <160692147@qq.com>
2020-07-28 20:43:03 +0800

    [spring-boot-employee][spike lu] feature: add getAllEmployees method and screenshot result.

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -2,6 +2,8 @@ package com.thoughtworks.springbootemployee.controller;
 
 import com.thoughtworks.springbootemployee.model.Company;
 import com.thoughtworks.springbootemployee.model.Employee;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +13,11 @@ import java.util.List;
 @RestController
 @RequestMapping("/employees")
 public class EmployeeController {
+    @GetMapping
+    public List<Employee> getAllEmployees(){
+        List<Employee> employees = createNewEmployees();
+        return employees;
+    }
 
 
     private List<Employee> createNewEmployees() {
