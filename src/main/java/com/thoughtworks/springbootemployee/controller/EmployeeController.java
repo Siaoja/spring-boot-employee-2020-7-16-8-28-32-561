77b658b2188d50cee0d75c643759d59f7a193b2f
Siaoja <160692147@qq.com>
2020-07-28 20:46:31 +0800

    [spring-boot-employee][spike lu] feature: add getEmployeeById method and screenshot result.

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -2,6 +2,7 @@ package com.thoughtworks.springbootemployee.controller;
 
 import com.thoughtworks.springbootemployee.model.Employee;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +17,16 @@ public class EmployeeController {
         return createNewEmployees();
     }
 
+    @GetMapping("/{id}")
+    public Employee getEmployeeById(@PathVariable int id){
+        List<Employee> employees = createNewEmployees();
+        for(Employee employee : employees){
+            if(employee.getId() == id){
+                return employee;
+            }
+        }
+        return null;
+    }
 
     private List<Employee> createNewEmployees() {
         List<Employee> employees = new ArrayList<>();
