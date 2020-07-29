27fedde18c020801fabb328e4521045ec2a61454
Siaoja <160692147@qq.com>
2020-07-29 21:09:30 +0800

    [spring-boot-employee][spike lu] feature: add test method should_return_companies_when_getAll_given_none

---
+++ src/test/java/com/thoughtworks/springbootemployee/service/CompanyServiceTest.java
@@ -0,0 +1,31 @@
+package com.thoughtworks.springbootemployee.service;
+
+import com.thoughtworks.springbootemployee.model.Company;
+import com.thoughtworks.springbootemployee.repository.CompanyRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.BDDMockito.given;
+
+public class CompanyServiceTest {
+    @Test
+    void should_return_companies_when_getAll_given_none() {
+        //given
+        List<Company> companies = new ArrayList<>();
+        companies.add(new Company());
+        companies.add(new Company());
+        CompanyRepository companyRepository = Mockito.mock(CompanyRepository.class);
+        given(companyRepository.findAll()).willReturn(companies);
+
+        //when
+        CompanyService companyService = new CompanyService(companyRepository);
+        List<Company> returnCompanies = companyService.getAllCompanies();
+
+        //then
+        assertEquals(2,returnCompanies.size());
+    }
+}
