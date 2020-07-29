b27f045360782bd041f2f9d7ff2b13949b3d598a
Siaoja <160692147@qq.com>
2020-07-29 21:53:50 +0800

    [spring-boot-employee][spike lu] refactor: refactor EmployeeController.

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -17,84 +17,37 @@ public class EmployeeController {
 
     @GetMapping
     public List<Employee> getAllEmployees() {
-        return createNewEmployees();
+        return employeeService.getAllEmployees();
     }
 
     @GetMapping("/{id}")
-    public Employee getEmployeeById(@PathVariable int id) {
-        List<Employee> employees = createNewEmployees();
-        for (Employee employee : employees) {
-            if (employee.getId() == id) {
-                return employee;
-            }
-        }
-        return null;
+    public Employee getEmployeeById(@PathVariable Integer id) {
+        return employeeService.getEmployeeByID(id);
     }
 
     @GetMapping(params = {"page", "pageSize"})
-    public List<Employee> getEmployeeByPage(@RequestParam(defaultValue = "null") int page, @RequestParam(defaultValue = "null") int pageSize) {
-        List<Employee> employees = createNewEmployees();
-        int beginIndex = (page - 1) * pageSize;
-        int endIndex = page * pageSize;
-        List<Employee> responseEmployees = new ArrayList<>();
-        for (; beginIndex < endIndex && beginIndex < employees.size(); beginIndex++) {
-            responseEmployees.add(employees.get(beginIndex));
-        }
-        return responseEmployees;
+    public List<Employee> getEmployeeByPage(@RequestParam() Integer page, @RequestParam() Integer pageSize) {
+        return employeeService.getAllEmployeesByPage(page,pageSize);
     }
 
     @GetMapping(params = {"gender"})
-    public List<Employee> getEmployeesByConditions(@RequestParam(defaultValue = "null") String gender) {
-        List<Employee> employees = createNewEmployees();
-        List<Employee> responseEmployees = new ArrayList<>();
-        for (Employee employee : employees) {
-            if (employee.getGender().equals(gender)) {
-                responseEmployees.add(employee);
-            }
-        }
-        return responseEmployees;
+    public List<Employee> getEmployeesByConditions(@RequestParam() String gender) {
+        return employeeService.getEmployeesByGender(gender);
     }
 
     @PostMapping
     public Employee addEmployee(@RequestBody Employee employee) {
-        return new Employee(employee.getId(), employee.getName(), employee.getAge(), employee.getGender(), employee.getSalary());
+        return employeeService.addEmployee(employee);
     }
 
     @PutMapping("/{id}")
-    public Employee updateEmployee(@PathVariable int id, @RequestBody Employee updateEmployee) {
-        List<Employee> employees = createNewEmployees();
-        for (Employee employee : employees) {
-            if (employee.getId() == id) {
-                employee.setAge(updateEmployee.getAge());
-                employee.setGender(updateEmployee.getGender());
-                employee.setName(updateEmployee.getName());
-                employee.setSalary(updateEmployee.getSalary());
-                return employee;
-            }
-        }
-        return null;
+    public Employee updateEmployee(@PathVariable Integer id, @RequestBody Employee updateEmployee) {
+        return employeeService.updateEmployee(id,updateEmployee);
     }
 
     @DeleteMapping("/{id}")
-    public Employee deleteEmployee(@PathVariable int id) {
-        List<Employee> employees = createNewEmployees();
-        for (int index = 0, length = employees.size(); index < length; index++) {
-            if (employees.get(index).getId() == id) {
-                return employees.remove(index);
-            }
-        }
-        return null;
+    public Employee deleteEmployee(@PathVariable Integer id) {
+        return employeeService.deleteEmployee(id);
     }
 
-    private List<Employee> createNewEmployees() {
-        List<Employee> employees = new ArrayList<>();
-        employees.add(new Employee(1, "alibaba1", 20, "male", 6000));
-        employees.add(new Employee(2, "tengxun2", 19, "female", 7000));
-        employees.add(new Employee(3, "alibaba3", 19, "male", 8000));
-        employees.add(new Employee(4, "alibaba4", 20, "male", 9000));
-        employees.add(new Employee(5, "tengxun5", 19, "female", 10000));
-        employees.add(new Employee(6, "alibaba6", 19, "male", 11000));
-        employees.add(new Employee(7, "alibaba7", 19, "male", 12000));
-        return employees;
-    }
 }
