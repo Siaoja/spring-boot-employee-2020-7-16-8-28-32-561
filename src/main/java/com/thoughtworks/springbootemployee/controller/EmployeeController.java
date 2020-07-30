f18820c756e4c7924262bdee52d58cc542319d58
Siaoja <160692147@qq.com>
2020-07-30 08:36:45 +0800

    [spring-boot-employee][spike lu] refactor: add post, put, delete option response code.

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.thoughtworks.springbootemployee.controller;
 import com.thoughtworks.springbootemployee.model.Employee;
 import com.thoughtworks.springbootemployee.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -36,16 +37,19 @@ public class EmployeeController {
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public Employee addEmployee(@RequestBody Employee employee) {
         return employeeService.addEmployee(employee);
     }
 
     @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public Employee updateEmployee(@PathVariable Integer id, @RequestBody Employee updateEmployee) {
         return employeeService.updateEmployee(id,updateEmployee);
     }
 
     @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public Employee deleteEmployee(@PathVariable Integer id) {
         return employeeService.deleteEmployee(id);
     }
