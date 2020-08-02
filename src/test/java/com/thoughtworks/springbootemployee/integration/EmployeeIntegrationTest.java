f9ab47a0dece12b71142ea97ea67fc71c07cc367
Siaoja <1610692147@qq.com>
2020-08-02 12:31:18 +0800

    [company-employee][spike] feature: add employee integration test method should_get_employee_when_hit_get_employees_endpoint_given_nothing.

---
+++ src/test/java/com/thoughtworks/springbootemployee/integration/EmployeeIntegrationTest.java
@@ -0,0 +1,59 @@
+package com.thoughtworks.springbootemployee.integration;
+
+import com.thoughtworks.springbootemployee.model.Company;
+import com.thoughtworks.springbootemployee.model.Employee;
+import com.thoughtworks.springbootemployee.repository.CompanyRepository;
+import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Collections;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class EmployeeIntegrationTest {
+    @Autowired
+    EmployeeRepository employeeRepository;
+    @Autowired
+    CompanyRepository companyRepository;
+    @Autowired
+    MockMvc mockMvc;
+
+    @AfterEach
+    void tearDown() {
+        employeeRepository.deleteAll();
+        companyRepository.deleteAll();
+    }
+
+    @Test
+    void should_get_employee_when_hit_get_employees_endpoint_given_nothing() throws Exception {
+        //given
+        Company company = new Company(null, "alibaba", 200, Collections.emptyList());
+        company = companyRepository.save(company);
+        Employee employee = new Employee(null, "alibaba1", 18, "male", 1000, company.getId());
+        employee = employeeRepository.save(employee);
+
+        //when
+        mockMvc.perform(get("/employees"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].id").isNumber())
+                .andExpect(jsonPath("$[0].name").value(employee.getName()))
+                .andExpect(jsonPath("$[0].age").value(employee.getAge()))
+                .andExpect(jsonPath("$[0].salary").value(employee.getSalary()))
+                .andExpect(jsonPath("$[0].gender").value(employee.getGender()))
+                .andExpect(jsonPath("$[0].companyId").value(employee.getCompanyId()));
+
+
+        //then
+    }
+}
